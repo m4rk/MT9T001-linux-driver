2239498a219831200bbfc2f8b6d0543f13974d7a
Markellos Orfanos <mark.orfanos@gmail.com>
2014-06-27 21:21:38 +0300

    added loadable module support to kernel(insmod/rmmod), modules (un)load successfully

--- snapgear-2.6-p42/linux-2.6.21.1/drivers/media/ceid_camera.c
+++ snapgear-2.6-p42/linux-2.6.21.1/drivers/media/ceid_camera.c
@@ -1,17 +1,19 @@
+/* Mandatory Driver module header files */
 #include <linux/init.h>
 #include <linux/module.h>
-MODULE_LICENSE("Dual BDS/GPL");
 
-static int __init initialization(void)
+MODULE_LICENSE("GPL");
+
+static int __init ceid_camera_init(void)
 {
 	printk(KERN_ALERT "CEID_CAMERA: module loaded\n");
 	return 0;
 }
 
-static void __exit cleanup(void)
+static void __exit ceid_camera_exit(void)
 {
 	printk(KERN_ALERT "CEID_CAMERA: module unloaded\n");
 }
 
-module_init(initialization);
-module_exit(cleanup);
+module_init(ceid_camera_init);
+module_exit(ceid_camera_exit);
